6bfc49043a78d5195e56200e764718c8286d5ff3
markuskaufmann87 <markus.kaufmann@edu.campus02.at>
2020-06-29 18:31:15 +0200

    created own Exception for reaching max size of queue elements

---
+++ src/main/java/at/fhj/iit/MaxSizeException.java
@@ -0,0 +1,14 @@
+package at.fhj.iit;
+
+/**
+ * MaxSizeException informs the user when the maximum size of the Queue
+ * is reached. Thrown with special Error message!
+ * @author: Markus Kaufmann
+ * @version %I%, %G%
+ */
+public class MaxSizeException extends Exception{
+
+    MaxSizeException(){
+        super("No adding possible! Maximum Size reached!");
+    }
+}
