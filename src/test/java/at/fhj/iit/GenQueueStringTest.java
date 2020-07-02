0d20ee9226d6fa8e3e0a19fb0939559156188bf6
LukasDoppelhofer <lukas.doppelhofer@edu.campus02.at>
2020-07-02 11:07:52 +0200

    removed result tags

--- src/test/java/at/fhj/iit/GenQueueStringTest.java
+++ src/test/java/at/fhj/iit/GenQueueStringTest.java
@@ -41,7 +41,7 @@ public class GenQueueStringTest {
 
     /**
      * testing the constructor of {@link GenQueue} with maxSize = 2
-     * @result equals 2 because maxSize is 2
+     * result: equals 2 because maxSize is 2
      */
     @Test
     @DisplayName("Testing constructor GenQueue")
@@ -51,7 +51,7 @@ public class GenQueueStringTest {
 
     /**
      * testing the default constructor of {@link GenQueue}
-     * @result default value of maxSize should be 5
+     * result: default value of maxSize should be 5
      */
     @Test
     @DisplayName("Testing default constructor GenQueue")
@@ -62,8 +62,8 @@ public class GenQueueStringTest {
     /**
      * testing {@link GenQueue#enqueue(Object)}
      * checks if return boolean is false if maxSize of queue is reached
-     * @result {@link MaxSizeException} thrown if maxSize of queue is reached!
-     * @result is "first" because head Element is "first" (FiFo)
+     * result: {@link MaxSizeException} thrown if maxSize of queue is reached!
+     * result: is "first" because head Element is "first" (FiFo)
      */
     @Test
     @DisplayName("Testing enqueue method")
@@ -76,8 +76,8 @@ public class GenQueueStringTest {
 
     /**
      * testing {@link GenQueue#getHeadElement()}
-     * @result {@link NoSuchElementException} if queue is empty
-     * @result "first" because it is head element of queue
+     * result: {@link NoSuchElementException} if queue is empty
+     * result: "first" because it is head element of queue
      */
     @Test
     @DisplayName(("Testing getHeadElement()"))
@@ -91,9 +91,9 @@ public class GenQueueStringTest {
      * testing {@link GenQueue#dequeue()}  method
      * checks if method throws exception if the queue is empty
      * checks if method return the head element and remove it
-     * @result {@link NoSuchElementException} thrown because {@link GenQueueIntegerTest#queue1} is empty
-     * @result {@link GenQueueIntegerTest#queue} "first" expected because head element is "first"
-     * @result {@link GenQueueIntegerTest#queue} "1" expected because queueSize should be "1" after removing 1 of 2 elements
+     * result: {@link NoSuchElementException} thrown because {@link GenQueueIntegerTest#queue1} is empty
+     * result: {@link GenQueueIntegerTest#queue} "first" expected because head element is "first"
+     * result: {@link GenQueueIntegerTest#queue} "1" expected because queueSize should be "1" after removing 1 of 2 elements
      */
     @Test
     @DisplayName("Testing dequeue method")
@@ -108,7 +108,7 @@ public class GenQueueStringTest {
 
     /**
      * testing {@link GenQueue#getElements()}
-     * @result String with content "[first, second]", because the 2 Elements are "first" and "second".
+     * result: String with content "[first, second]", because the 2 Elements are "first" and "second".
      */
     @Test
     @DisplayName("Testing getElements method")
@@ -120,7 +120,7 @@ public class GenQueueStringTest {
 
     /**
      * testing {@link GenQueue#setMaxSize(int)}
-     * @result 4 because maxSize set to 4.
+     * result: 4 because maxSize set to 4.
      */
     @Test
     @DisplayName("Testing setMaxSize method")
