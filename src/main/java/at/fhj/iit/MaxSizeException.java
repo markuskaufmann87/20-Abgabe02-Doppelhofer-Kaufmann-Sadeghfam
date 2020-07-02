40979f9609df262f9471824b9d2ea519706108db
LukasDoppelhofer <lukas.doppelhofer@edu.campus02.at>
2020-07-02 11:09:19 +0200

    changed author tag

--- src/main/java/at/fhj/iit/MaxSizeException.java
+++ src/main/java/at/fhj/iit/MaxSizeException.java
@@ -3,7 +3,7 @@ package at.fhj.iit;
 /**
  * MaxSizeException informs the user when the maximum size of the Queue
  * is reached. Thrown with special Error message!
- * @author: Markus Kaufmann
+ * @author Markus Kaufmann
  * @version %I%, %G%
  */
 public class MaxSizeException extends Exception{
