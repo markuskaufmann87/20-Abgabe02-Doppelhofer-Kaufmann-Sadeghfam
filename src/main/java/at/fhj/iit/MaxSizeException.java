689abd8f68c5ae5abafc888a67e10cf861b0cb29
markuskaufmann87 <markus.kaufmann@edu.campus02.at>
2020-07-03 13:29:06 +0200

    changed inheritance to extension of RuntimeException

--- src/main/java/at/fhj/iit/MaxSizeException.java
+++ src/main/java/at/fhj/iit/MaxSizeException.java
@@ -3,12 +3,17 @@ package at.fhj.iit;
 /**
  * MaxSizeException informs the user when the maximum size of the Queue
  * is reached. Thrown with special Error message!
+ *
  * @author Markus Kaufmann
  * @version %I%, %G%
  */
-public class MaxSizeException extends Exception{
+public class MaxSizeException extends RuntimeException {
 
-    MaxSizeException(){
+    MaxSizeException() {
         super("No adding possible! Maximum Size reached!");
+    }
+
+    MaxSizeException(String text) {
+        super(text);
     }
 }
