b3b25028dce964c7c84367a11f30cfcb8950e501
markuskaufmann87 <markus.kaufmann@edu.campus02.at>
2020-07-03 13:35:17 +0200

    throwing Exceptions deleted and optimized JavaDoc comments

--- src/test/java/at/fhj/iit/GenQueueIntegerTest.java
+++ src/test/java/at/fhj/iit/GenQueueIntegerTest.java
@@ -12,6 +12,7 @@ import static org.junit.jupiter.api.Assertions.*;
 /**
  * Class tests the GenQueue - generic version of Queue
  * with an Integer data type.
+ *
  * @author Markus Kaufmann
  * @author Lukas Doppelhofer
  * @version %I%, %G%
@@ -25,7 +26,7 @@ public class GenQueueIntegerTest {
      * inits 2 instances of {@link GenQueue} with type Integer
      */
     @BeforeEach
-    void setup() throws MaxSizeException {
+    void setup() {
         queue = new GenQueue<Integer>(2);
         queue1 = new GenQueue<Integer>();
     }
@@ -34,56 +35,64 @@ public class GenQueueIntegerTest {
      * resets the two instances of {@link GenQueue} after each test
      */
     @AfterEach
-    void tearDown(){
+    void tearDown() {
         queue = null;
         queue1 = null;
     }
 
     /**
      * testing the constructor of {@link GenQueue} with maxSize = 2
-     * result: equals 2 because maxSize is 2
+     *
+     * @result equals 2 because maxSize is 2
      */
     @Test
     @DisplayName("Testing constructor GenQueue")
-    public void testConstructorGenQueue(){
+    public void testConstructorGenQueue() {
 
-        assertEquals(2,queue.getMaxSize());
+        assertEquals(2, queue.getMaxSize());
     }
 
     /**
      * testing the default constructor of {@link GenQueue}
-     * result: default value of maxSize should be 5
+     *
+     * @result default value of maxSize should be 5
      */
     @Test
     @DisplayName("Testing default constructor GenQueue")
-    public void testDefaultConstructorGenQueue(){
-        assertEquals(5,queue1.getMaxSize());
+    public void testDefaultConstructorGenQueue() {
+        assertEquals(5, queue1.getMaxSize());
     }
 
     /**
      * testing {@link GenQueue#enqueue(Object)}
      * checks if return boolean is false if maxSize of queue is reached
-     * result: {@link MaxSizeException} thrown if maxSize of queue is reached!
-     * result: is 3 because head Element is 3 (FiFo)
+     *
+     * @throws MaxSizeException thrown if maxSize of queue is reached!
+     * @result is 3 because head Element is 3 (FiFo)
      */
     @Test
     @DisplayName("Testing enqueue method")
-    public void testEnqueue() throws MaxSizeException {
+    public void testEnqueue() {
         queue.enqueue(3);
         queue.enqueue(10);
-        assertThrows(MaxSizeException.class, ()->{queue.enqueue(6);}, "Queue's max size reached");
-        assertEquals(3,queue.getHeadElement());
+        assertThrows(MaxSizeException.class, () -> {
+            queue.enqueue(6);
+        }, "Queue's max size reached");
+        assertEquals(3, queue.getHeadElement());
     }
 
     /**
      * testing {@link GenQueue#getHeadElement()}
-     * result: {@link NoSuchElementException} if queue is empty
-     * result: 3 because 3 is head element of queue
+     *
+     * @throws NoSuchElementException if queue is empty
+     * @result 3 because 3 is head element of queue
      */
     @Test
     @DisplayName(("Testing getHeadElement()"))
-    public void testGetHeadElement() throws MaxSizeException{
-        assertThrows(NoSuchElementException.class, ()->{queue1.getHeadElement();}, "Queue is empty");
+    public void testGetHeadElement() {
+        assertThrows(NoSuchElementException.class, () -> {
+            queue1.getHeadElement();
+        }, "Queue is empty");
         queue.enqueue(3);
         assertEquals(3, queue.getHeadElement());
     }
@@ -92,40 +101,45 @@ public class GenQueueIntegerTest {
      * testing {@link GenQueue#dequeue()}  method
      * checks if method throws exception if the queue is empty
      * checks if method return the head element and remove it
-     * result: {@link NoSuchElementException} thrown because {@link GenQueueIntegerTest#queue1} is empty
-     * result: {@link GenQueueIntegerTest#queue} "3" expected because head element is "3"
-     * result: {@link GenQueueIntegerTest#queue} "1" expected because queueSize should be "1" after removing 1 of 2 elements
+     *
+     * @throws NoSuchElementException thrown because {@link GenQueueIntegerTest#queue1} is empty
+     * @result {@link GenQueueIntegerTest#queue} "3" expected because head element is "3"
+     * @result {@link GenQueueIntegerTest#queue} "1" expected because queueSize should be "1" after removing 1 of 2 elements
      */
     @Test
     @DisplayName("Testing dequeue method")
-    public void testDequeue() throws MaxSizeException{
-        assertThrows(NoSuchElementException.class, ()->{queue1.dequeue();}, "Queue is empty");
+    public void testDequeue() {
+        assertThrows(NoSuchElementException.class, () -> {
+            queue1.dequeue();
+        }, "Queue is empty");
         queue.enqueue(3);
         queue.enqueue(10);
-        assertEquals(3,queue.dequeue());
+        assertEquals(3, queue.dequeue());
 
-        assertEquals(1,queue.getQueueSize());
+        assertEquals(1, queue.getQueueSize());
     }
 
     /**
      * testing {@link GenQueue#getElements()}
-     * result: String with content "[3, 10]", because the 2 Elements are 3 and 10.
+     *
+     * @result String with content "[3, 10]", because the 2 Elements are 3 and 10.
      */
     @Test
     @DisplayName("Testing getElements method")
-    public void testGetElements() throws MaxSizeException{
+    public void testGetElements() throws MaxSizeException {
         queue.enqueue(3);
         queue.enqueue(10);
-        assertEquals("[3, 10]",queue.getElements().toString());
+        assertEquals("[3, 10]", queue.getElements().toString());
     }
 
     /**
      * testing {@link GenQueue#setMaxSize(int)}
-     * result: 4 because maxSize set to 4.
+     *
+     * @result 4 because maxSize set to 4.
      */
     @Test
     @DisplayName("Testing setMaxSize method")
-    public void testSetMaxSize(){
+    public void testSetMaxSize() {
         queue1.setMaxSize(4);
         assertEquals(4, queue1.getMaxSize());
     }
--- src/test/java/at/fhj/iit/GenQueueStringTest.java
+++ src/test/java/at/fhj/iit/GenQueueStringTest.java
@@ -12,6 +12,7 @@ import static org.junit.jupiter.api.Assertions.*;
 /**
  * Class tests the GenQueue - generic version of Queue
  * with a String data type.
+ *
  * @author Markus Kaufmann
  * @author Mehrad Sadeghfam
  * @version %I%, %G%
@@ -25,7 +26,7 @@ public class GenQueueStringTest {
      * inits 2 instances of {@link GenQueue} with type String
      */
     @BeforeEach
-    void setup() throws MaxSizeException {
+    void setup() {
         queue = new GenQueue<String>(2);
         queue1 = new GenQueue<String>();
     }
@@ -34,55 +35,63 @@ public class GenQueueStringTest {
      * resets the two instances of {@link GenQueue} after each test
      */
     @AfterEach
-    void tearDown(){
+    void tearDown() {
         queue = null;
         queue1 = null;
     }
 
     /**
      * testing the constructor of {@link GenQueue} with maxSize = 2
-     * result: equals 2 because maxSize is 2
+     *
+     * @result equals 2 because maxSize is 2
      */
     @Test
     @DisplayName("Testing constructor GenQueue")
-    public void testConstructorGenQueue(){
-        assertEquals(2,queue.getMaxSize());
+    public void testConstructorGenQueue() {
+        assertEquals(2, queue.getMaxSize());
     }
 
     /**
      * testing the default constructor of {@link GenQueue}
-     * result: default value of maxSize should be 5
+     *
+     * @result default value of maxSize should be 5
      */
     @Test
     @DisplayName("Testing default constructor GenQueue")
-    public void testDefaultConstructorGenQueue(){
-        assertEquals(5,queue1.getMaxSize());
+    public void testDefaultConstructorGenQueue() {
+        assertEquals(5, queue1.getMaxSize());
     }
 
     /**
      * testing {@link GenQueue#enqueue(Object)}
      * checks if return boolean is false if maxSize of queue is reached
-     * result: {@link MaxSizeException} thrown if maxSize of queue is reached!
-     * result: is "first" because head Element is "first" (FiFo)
+     *
+     * @throws MaxSizeException thrown if maxSize of queue is reached!
+     * @result is "first" because head Element is "first" (FiFo)
      */
     @Test
     @DisplayName("Testing enqueue method")
-    public void testEnqueue() throws MaxSizeException {
+    public void testEnqueue() {
         queue.enqueue("first");
         queue.enqueue("second");
-        assertThrows(MaxSizeException.class, ()->{queue.enqueue("third");}, "Queue's max size reached");
-        assertEquals("first",queue.getHeadElement());
+        assertThrows(MaxSizeException.class, () -> {
+            queue.enqueue("third");
+        }, "Queue's max size reached");
+        assertEquals("first", queue.getHeadElement());
     }
 
     /**
      * testing {@link GenQueue#getHeadElement()}
-     * result: {@link NoSuchElementException} if queue is empty
-     * result: "first" because it is head element of queue
+     *
+     * @throws NoSuchElementException if queue is empty
+     * @result "first" because it is head element of queue
      */
     @Test
     @DisplayName(("Testing getHeadElement()"))
-    public void testGetHeadElement() throws MaxSizeException{
-        assertThrows(NoSuchElementException.class, ()->{queue1.getHeadElement();}, "Queue is empty");
+    public void testGetHeadElement() {
+        assertThrows(NoSuchElementException.class, () -> {
+            queue1.getHeadElement();
+        }, "Queue is empty");
         queue.enqueue("first");
         assertEquals("first", queue.getHeadElement());
     }
@@ -91,40 +100,45 @@ public class GenQueueStringTest {
      * testing {@link GenQueue#dequeue()}  method
      * checks if method throws exception if the queue is empty
      * checks if method return the head element and remove it
-     * result: {@link NoSuchElementException} thrown because {@link GenQueueIntegerTest#queue1} is empty
-     * result: {@link GenQueueIntegerTest#queue} "first" expected because head element is "first"
-     * result: {@link GenQueueIntegerTest#queue} "1" expected because queueSize should be "1" after removing 1 of 2 elements
+     *
+     * @throws NoSuchElementException thrown because {@link GenQueueIntegerTest#queue1} is empty
+     * @result {@link GenQueueIntegerTest#queue} "first" expected because head element is "first"
+     * @result {@link GenQueueIntegerTest#queue} "1" expected because queueSize should be "1" after removing 1 of 2 elements
      */
     @Test
     @DisplayName("Testing dequeue method")
-    public void testDequeue() throws MaxSizeException{
-        assertThrows(NoSuchElementException.class, ()->{queue1.dequeue();}, "Queue is empty");
+    public void testDequeue() {
+        assertThrows(NoSuchElementException.class, () -> {
+            queue1.dequeue();
+        }, "Queue is empty");
         queue.enqueue("first");
         queue.enqueue("second");
-        assertEquals("first",queue.dequeue());
+        assertEquals("first", queue.dequeue());
 
-        assertEquals(1,queue.getQueueSize());
+        assertEquals(1, queue.getQueueSize());
     }
 
     /**
      * testing {@link GenQueue#getElements()}
-     * result: String with content "[first, second]", because the 2 Elements are "first" and "second".
+     *
+     * @result String with content "[first, second]", because the 2 Elements are "first" and "second".
      */
     @Test
     @DisplayName("Testing getElements method")
-    public void testGetElements() throws MaxSizeException{
+    public void testGetElements() {
         queue.enqueue("first");
         queue.enqueue("second");
-        assertEquals("[first, second]",queue.getElements().toString());
+        assertEquals("[first, second]", queue.getElements().toString());
     }
 
     /**
      * testing {@link GenQueue#setMaxSize(int)}
-     * result: 4 because maxSize set to 4.
+     *
+     * @result 4 because maxSize set to 4.
      */
     @Test
     @DisplayName("Testing setMaxSize method")
-    public void testSetMaxSize(){
+    public void testSetMaxSize() {
         queue1.setMaxSize(4);
         assertEquals(4, queue1.getMaxSize());
     }
